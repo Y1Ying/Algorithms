b0ad000faf8dfc00b4ba89fe5befc74888a20a8b
Y1Ying <yinxin0476@vip.qq.com>
2017-04-12 21:30:44 +0800

    Algorithms

--- src/binaryTree/TreeToSequence.java
+++ src/binaryTree/TreeToSequence.java
@@ -1,5 +1,7 @@
 package binaryTree;
 
+import java.util.Stack;
+
 /**
  * 请用递归方式和非递归的方式实现二叉树的先序、中序和后序的遍历打印。
  * 
@@ -20,6 +22,7 @@ public class TreeToSequence {
 		}
 	}
 
+	// ===============递归方法===============
 	// 先序遍历
 	public static void preOrderRecur(Node root) {
 		if (root == null) {
@@ -48,6 +51,87 @@ public class TreeToSequence {
 		preOrderRecur(root.left);
 		preOrderRecur(root.right);
 		System.out.println(root.value + " ");
+	}
+
+	// ===============非递归方法===============
+
+	// 非递归先序
+	public static void preOrderUnRecur(Node head) {
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			stack.add(head);
+			while (!stack.isEmpty()) {
+				head = stack.pop();
+				System.out.println(head.value + " ");
+				if (head.right != null) {
+					stack.push(head.right);
+				}
+				if (head.left != null) {
+					stack.push(head.left);
+				}
+			}
+		}
+		System.out.println();
+	}
+
+	// 非递归中序
+	public static void inOrderUnRecur(Node head) {
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			while (!stack.isEmpty() || head != null) {
+				if (head != null) {
+					stack.push(head);
+					head = head.left;
+				} else {
+					head = stack.pop();
+					System.out.println(head.value + " ");
+					head = head.right;
+				}
+			}
+		}
+		System.out.println();
+	}
+
+	// 非递归后序=======使用2个栈
+	public static void posOrderUnRecur1(Node head) {
+		if (head != null) {
+			Stack<Node> s1 = new Stack<>();
+			Stack<Node> s2 = new Stack<>();
+			s1.push(head);
+			while (!s1.isEmpty()) {
+				head = s1.pop();
+				s2.push(head);
+				if (head.left != null) {
+					s1.push(head.left);
+				}
+				if (head.right != null) {
+					s1.push(head.right);
+				}
+			}
+			while (!s2.isEmpty()) {
+				System.out.println(s2.pop().value + " ");
+			}
+		}
+		System.out.println();
+	}
+
+	public void posOrderUnRecur2(Node h) {
+		if (h != null) {
+			Stack<Node> stack = new Stack<>();
+			stack.push(h);
+			Node c = null;
+			while (!stack.isEmpty()) {
+				c = stack.peek();
+				if (c.left != null && h != c.left && h != c.right) {
+					stack.push(c.left);
+				} else if (c.right != null && h != c.right) {
+					stack.push(c.right);
+				} else {
+					System.out.println(stack.pop().value + " ");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 	public static void main(String[] args) {
