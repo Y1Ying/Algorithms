d3ff8950b560bd7b40b1349b977a3b34caaa18ae
Y1Ying <yinxin0476@vip.qq.com>
2017-06-26 23:06:43 +0800

    Algorithms

--- src/list/Solution.java
+++ src/list/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
 		}
 	}
 
-	public Node copyListWithRand1(Node head) {
+	public static Node copyListWithRand1(Node head) {
 
 		if (head == null) {
 			return head;
@@ -39,5 +39,93 @@ public class Solution {
 			cur = cur.next;
 		}
 		return map.get(head);
+	}
+
+	public static Node copyListWithRand2(Node head) {
+		if (head == null) {
+			return null;
+		}
+		Node cur = head;
+		Node next = null;
+		// 复制链接每个节点
+		while (cur != null) {
+			next = cur.next;
+			cur.next = new Node(cur.value);
+			cur.next.next = next;
+			cur = next;
+		}
+		cur = head;
+		Node curCopy = null;
+		// 设置复制节点的rand指针
+		while (cur != null) {
+			next = cur.next.next;
+			curCopy = cur.next;
+			curCopy.rand = cur.rand != null ? cur.rand.next : null;
+			cur = next;
+		}
+		Node res = head.next;
+		cur = head;
+		// 拆分
+		while (cur != null) {
+			next = cur.next.next;
+			curCopy = cur.next;
+			cur.next = next;
+			curCopy.next = next != null ? next.next : null;
+			cur = next;
+		}
+		return res;
+	}
+
+	public static void printRandLinkedList(Node head) {
+		Node cur = head;
+		System.out.print("order: ");
+		while (cur != null) {
+			System.out.print(cur.value + " ");
+			cur = cur.next;
+		}
+		System.out.println();
+		cur = head;
+		System.out.print("rand:  ");
+		while (cur != null) {
+			System.out.print(cur.rand == null ? "- " : cur.rand.value + " ");
+			cur = cur.next;
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		Node head = null;
+		Node res1 = null;
+		Node res2 = null;
+		printRandLinkedList(head);
+		res1 = copyListWithRand1(head);
+		printRandLinkedList(res1);
+		res2 = copyListWithRand2(head);
+		printRandLinkedList(res2);
+		printRandLinkedList(head);
+		System.out.println("=========================");
+
+		head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+		head.next.next.next.next.next = new Node(6);
+
+		head.rand = head.next.next.next.next.next; // 1 -> 6
+		head.next.rand = head.next.next.next.next.next; // 2 -> 6
+		head.next.next.rand = head.next.next.next.next; // 3 -> 5
+		head.next.next.next.rand = head.next.next; // 4 -> 3
+		head.next.next.next.next.rand = null; // 5 -> null
+		head.next.next.next.next.next.rand = head.next.next.next; // 6 -> 4
+
+		printRandLinkedList(head);
+		res1 = copyListWithRand1(head);
+		printRandLinkedList(res1);
+		res2 = copyListWithRand2(head);
+		printRandLinkedList(res2);
+		printRandLinkedList(head);
+		System.out.println("=========================");
+
 	}
 }
