a081aef1ed8da47ae516342361f52f3bcd0f5443
Y1Ying <yinxin0476@vip.qq.com>
2017-07-12 23:33:07 +0800

    Algorithms

---
+++ src/list/selectionSortNode.java
@@ -0,0 +1,116 @@
+package list;
+
+public class selectionSortNode {
+
+	public static Node selectionSort(Node head) {
+		Node tail = null; // 排序部分尾部
+		Node cur = head; // 未排序部分头
+		Node smallPre = null; // 最小节点的前一个节点
+		Node small = null; // 最小的节点
+		while (cur != null) {
+			small = cur;
+			smallPre = getSmallestPreNode(cur);
+			if (smallPre != null) {
+				small = smallPre.next;
+				smallPre.next = small.next;
+			}
+			cur = cur == small ? cur.next : cur;
+			if (tail == null) {
+				head = small;
+			} else {
+				tail.next = small;
+			}
+			tail = small;
+		}
+		return head;
+	}
+
+	public static Node getSmallestPreNode(Node head) {
+		Node smallPre = null;
+		Node small = head;
+		Node pre = head;
+		Node cur = head.next;
+		while (cur != null) {
+			if (cur.value < small.value) {
+				smallPre = pre;
+				small = cur;
+			}
+			pre = cur;
+			cur = cur.next;
+		}
+		return smallPre;
+	}
+
+	public static void printLinkedList(Node head) {
+		System.out.print("Linked List: ");
+		while (head != null) {
+			System.out.print(head.value + " ");
+			head = head.next;
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		Node head = null;
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(1);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(1);
+		head.next = new Node(2);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(2);
+		head.next = new Node(1);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(1);
+		head.next = new Node(3);
+		head.next.next = new Node(2);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(2);
+		head.next = new Node(1);
+		head.next.next = new Node(3);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(2);
+		head.next = new Node(3);
+		head.next.next = new Node(1);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(3);
+		head.next = new Node(1);
+		head.next.next = new Node(2);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(3);
+		head.next = new Node(2);
+		head.next.next = new Node(1);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+		head = new Node(3);
+		head.next = new Node(1);
+		head.next.next = new Node(4);
+		head.next.next.next = new Node(2);
+		head = selectionSort(head);
+		printLinkedList(head);
+
+	}
+}
