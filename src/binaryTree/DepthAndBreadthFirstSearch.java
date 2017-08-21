6c8e136ed474303fb70af0aff4a843aa48c42383
Y1Ying <yinxin0476@vip.qq.com>
2017-08-22 00:01:21 +0800

    Algorithms

--- src/binaryTree/DepthAndBreadthFirstSearch.java
+++ src/binaryTree/DepthAndBreadthFirstSearch.java
@@ -1,5 +1,8 @@
 package binaryTree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -10,16 +13,35 @@ import java.util.Stack;
  */
 public class DepthAndBreadthFirstSearch {
 
+	// 深度遍历
 	public void DepthFirstSearch(Node head) {
 		Stack<Node> stack = new Stack<>();
 		stack.push(head);
 		while (!stack.isEmpty()) {
 			Node cur = stack.pop();
+			System.out.println(cur.value + " ");
 			if (cur.right != null) {
 				stack.push(cur.right);
 			}
 			if (cur.left != null) {
 				stack.push(cur.left);
+			}
+		}
+		System.out.println("\n");
+	}
+
+	// 广度遍历
+	public void BreadthFristSearch(Node head) {
+		Deque<Node> deque = new ArrayDeque<>();
+		deque.add(head);
+		while (!deque.isEmpty()) {
+			Node cur = deque.remove();
+			System.out.println(cur.value + " ");
+			if (cur.left != null) {
+				deque.add(cur.left);
+			}
+			if (cur.right != null) {
+				deque.add(cur.right);
 			}
 		}
 		System.out.println("\n");
