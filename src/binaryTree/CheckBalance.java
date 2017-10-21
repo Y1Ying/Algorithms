ec3c4b3b387176a15c887636bf9ca8765a3cb3c4
Y1Ying <yinxin0476@vip.qq.com>
2017-10-21 21:56:55 +0800

    Algorithms

--- src/binaryTree/CheckBalance.java
+++ src/binaryTree/CheckBalance.java
@@ -33,6 +33,27 @@ public class CheckBalance {
 		return Math.max(lh, rh);
 	}
 
+	public boolean IsBalanced_Solution(TreeNode root) {
+		if (root == null)
+			return true;
+
+		if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1)
+			return false;
+
+		return IsBalanced_Solution(root.left) && IsBalanced_Solution(root.right);
+
+	}
+
+	public int getHeight(TreeNode root) {
+		if (root == null)
+			return 0;
+		return max(getHeight(root.left), getHeight(root.right)) + 1;
+	}
+
+	private int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+
 	public static void main(String[] args) {
 		Node head = new Node(1);
 		head.left = new Node(2);
